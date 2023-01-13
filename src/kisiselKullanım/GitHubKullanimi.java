27f67fc61743014e67ec7f163d99991ae1ab0f19
hayatic <hayaticalcoban@gmail.com>
2023-01-13 22:46:38 +0300

    ilk değişiklik

--- src/kisiselKullanım/GitHubKullanimi.java
+++ src/kisiselKullanım/GitHubKullanimi.java
@@ -3,5 +3,6 @@ package kisiselKullanım;
 public class GitHubKullanimi {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("That's it");
     }
 }
